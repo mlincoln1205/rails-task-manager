8669b0a0167ae0a7a10d7372e4d7034584b222df
Lincoln Marcolongo <mlincoln1205@gmail.com>
2020-11-17 20:55:31 +0100

    Task Manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+    @tasks.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+  end
+
+  def update
+    @tasks.update(task_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @tasks.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+  private
+
+  def set_task
+    @tasks = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Mostra formulario para adicionar novas tasks
+  get "tasks/new", to: "tasks#new", as: :new
+  # Mostra detalhes das tasks
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Lista todas as tasks
+  get "tasks", to: "tasks#index"
+  # mostra formulário para editar tasks
+  get "tasks/.:id/edit", to: "tasks#edit"
+
+  # Cria novas Tasks
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
